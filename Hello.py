c3a85f565b1a82176b1546e78405abe97de4c390
KimberlySalazarB <118402135+KimberlySalazarB@users.noreply.github.com>
2024-03-16 19:09:34 +0000

    cambio

--- Hello.py
+++ Hello.py
@@ -31,7 +31,7 @@ import sklearn
 from sklearn.exceptions import InconsistentVersionWarning
 import warnings
 from pandasai import SmartDataframe
-from pandasai.llm.openai import OpenAI
+from pandasai.llm import OpenAI
 
 
 # Función para obtener el contenido de un archivo desde una URL
